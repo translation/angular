01619a64c136eab9bc6383f40740960d94957d7f
Simon <simon@corellia.be>
2022-09-02 16:05:48 +0200

    end clean sample

--- sample/src/app/app.component.ts
+++ sample/src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   minutes = 0;
   gender = 'female';
+
+  helloSourceFromJS = $localize `Hello source from JS`;
+  helloKeyFromJS = $localize`:@@TIO_MYAPP_HelloKeyJS: Hello key from JS`;
+
   fly = true;
   logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+
   inc(i: number) {
     this.minutes = Math.min(5, Math.max(0, this.minutes + i));
   }

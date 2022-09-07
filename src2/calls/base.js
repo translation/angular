37478c4b8b99a2c0f14f61a6de37d8a957dcbe5a
Michaël Hoste <michael.hoste@gmail.com>
2022-09-08 00:34:41 +0200

    2 importants informations in commit description
    * First try of XML parsing with `@xmldom/xmldom`. Don't use it! Use something like `tXml`, `fast-xml-parser` or other solution that don't mess with the XML in source and target nodes. Cf. https://stackoverflow.com/questions/64340617/issue-parsing-xliff-with-node-xml-parser
    
    * Revert back extract-i18n to the native simpler solution without `ng-extract-i18n-merge` (with xlif or xlif2 ?)

--- src2/calls/base.js
+++ src2/calls/base.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const { DOMParser, XMLSerializer } = require('@xmldom/xmldom')
+
 
 class Base {
   constructor(configFile) {
@@ -16,8 +18,58 @@ class Base {
   }
 
   targetLocales() {
-    return this.options()['target_locales'].map((locale) => locale.trim())
+    return this.options()['target_locales'].map(locale => locale.trim())
+  }
+
+  convertXmlUnitsToSegments(xmlUnits) {
+    //console.log(xmlUnits)
+    return Array.from(xmlUnits).map(xmlUnit =>
+      this.convertXmlUnitToSegment(xmlUnit)
+    )
+  }
+
+  convertXmlUnitToSegment(xmlUnit) {
+    //console.log(xmlUnit)
+
+    return {
+      type: 'source',
+      source: new XMLSerializer().serializeToString(
+        xmlUnit.getElementsByTagName("source")[0].childNodes[1]
+      )
+    }
+
+
+    // y = x.childNodes[0];
+    // z = y.nodeValue;
+
+    // return {
+    //   type: 'source',
+    //   source:
+    //   target:
+    //   context:
+    //   comment:
+    //   references:
+    // }
+
+    // const sourceElement = transUnits.getElementsByTagName('source')[0];
+    // if (sourceElement) {
+    //     this.source = (0, utils_1.getXMLElementToString)('source', sourceElement);
+    // }
+    // // We need to process first the source and then the target
+    // this.addTarget(transUnits);
+
+    // <trans-unit id="introductionHeader" datatype="html">
+    //   <source> Hello i18n! </source>
+    //   <target state="new"> Hello i18n! </target>
+    //   <context-group purpose="location">
+    //     <context context-type="sourcefile">src/app/app.component.html</context>
+    //     <context context-type="linenumber">1,3</context>
+    //   </context-group>
+    //   <note priority="1" from="description">An introduction header for this sample</note>
+    //   <note priority="1" from="meaning">User welcome</note>
+    // </trans-unit>
   }
 }
 
 module.exports = Base
+
--- src2/calls/init.js
+++ src2/calls/init.js
@@ -1,5 +1,8 @@
 const Base = require('./base')
 
+const fs = require('fs')
+const { DOMParser, XMLSerializer } = require('@xmldom/xmldom')
+
 class Init extends Base {
   constructor(configFile) {
     super(configFile)
@@ -10,7 +13,19 @@ class Init extends Base {
     console.log(this.configFile)
     console.log(this.sourceLocale())
     console.log(this.targetLocales())
+
+    const sourceXml = new DOMParser().parseFromString(
+      fs.readFileSync('./src/locale/messages.xlf', 'utf8'),
+      'text/xml'
+     )
+
+    const sourceXmlUnits = sourceXml.getElementsByTagName('trans-unit')
+    const sourceSegments = this.convertXmlUnitsToSegments(sourceXmlUnits)
+
+    console.log(sourceSegments)
   }
 }
 
 module.exports = Init
+
+

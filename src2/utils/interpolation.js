38b078441238d054267cbdc2a5543c56917dc692
Michaël Hoste <michael.hoste@gmail.com>
2022-09-12 21:04:12 +0200

    First `Interpolation.escape`/`Interpolation.unescape` implementation

---
+++ src2/utils/interpolation.js
@@ -0,0 +1,77 @@
+class Interpolation {
+  // Convert
+  // 'Hello <x id="INTERPOLATION"/> and <x id="INTERPOLATION"/>'
+  // to
+  //'Hello <x0> and <x1>'
+  //----
+  // Convert
+  // 'Hello <x id="INTERPOLATION" equiv-text="{{ name }}"/> and <x id="INTERPOLATION" equiv-text="{{ otherName }}"/>'
+  // to
+  //'Hello <x0> and <x1>'
+  static escape(text) {
+    const regexp         = /<x.*?\/>/g
+    const interpolations = text.match(regexp) || [] // Because JS is sad and returns null instead of [] if no results
+    let   escapedText    = `${text}`
+
+    interpolations.forEach((interpolation, index) =>
+      escapedText = escapedText.replace(interpolation, `<x${index}>`)
+    )
+
+    return {
+      text:           escapedText,
+      interpolations: interpolations
+    }
+  }
+
+  // Convert
+  //'Hello <x0> and <x1>' and ['<x id="INTERPOLATION"/>', '<x id="INTERPOLATION"/>']
+  // to
+  // 'Hello <x id="INTERPOLATION"/> and <x id="INTERPOLATION"/>'
+  //----
+  // Convert
+  //'Hello <x1> and <x0>' and ['<x id="INTERPOLATION" equiv-text="{{ name }}"/>', '<x id="INTERPOLATION" equiv-text="{{ otherName }}"/>']
+  // to
+  // 'Hello <x id="INTERPOLATION" equiv-text="{{ otherName }}"/> and <x id="INTERPOLATION" equiv-text="{{ name }}"/>
+  static unescape(escapedText, interpolations) {
+    const regexp               = /<x\d+?>/g
+    const simpleInterpolations = escapedText.match(regexp) || []
+
+    let text = `${escapedText}`
+
+    simpleInterpolations.forEach((simpleInterpolation) => {
+      const index = parseInt(simpleInterpolation.replace('<x', '').replace('/>', ''))
+      text = text.replace(simpleInterpolation, (interpolations[index] || ''))
+    })
+
+    return text
+  }
+}
+
+module.exports = Interpolation
+
+
+
+
+
+
+
+// const result = Interpolation.escape('Hello <x id="INTERPOLATION" equiv-text="{{ name }}"/> and <x id="INTERPOLATION" equiv-text="{{ otherName }}"/>')
+
+// {
+//   text: 'Hello <x0> and <x1>',
+//   interpolations: [
+//     '<x id="INTERPOLATION" equiv-text="{{ name }}"/>',
+//     '<x id="INTERPOLATION" equiv-text="{{ otherName }}"/>'
+//   ]
+// }
+
+// Interpolation.unescape('Hello <x0> and <x1>', [
+//   '<x id="INTERPOLATION" equiv-text="{{ name }}"/>',
+//   '<x id="INTERPOLATION" equiv-text="{{ otherName }}"/>'
+// ])
+
+// Interpolation.unescape('Hello <x1> and <x0>', [
+//   '<x id="INTERPOLATION" equiv-text="{{ name }}"/>',
+//   '<x id="INTERPOLATION" equiv-text="{{ otherName }}"/>'
+// ])
+

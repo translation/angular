800d9824578a0fc9a6d74170527c306322979be2
Didier Toussaint <didier.toussaint.trad@gmail.com>
2022-09-19 11:29:25 +0200

    Remove minimist from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-const minimist = require('minimist')
-const Init     = require('./calls/init')
-const Sync     = require('./calls/sync')
+const Init = require('./calls/init')
+const Sync = require('./calls/sync')
 
-const args = minimist(process.argv.slice(2))
-let flags  = Object.assign({}, args)
-delete flags._
+const args   = process.argv.slice(2)
+const action = args[0]
+const config = args.find(arg => arg.includes('--config'))?.split('=')[1]
+const flags  = {
+  readonly: args.includes('--readonly'),
+  purge:    args.includes('--purge')
+}
 
-if (args._.includes('init')) {
-  let initService = new Init(args.config, flags)
+if (action == 'init') {
+  let initService = new Init(config, flags)
   initService.run()
 }
-else if (args._.includes('sync')) {
-  let syncService = new Sync(args.config, flags)
+else if (action == 'sync') {
+  let syncService = new Sync(config, flags)
   syncService.run()
 }
 else {
-  console.log('nothing')
+  console.log('No action selected')
 }

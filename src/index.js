06729be13530960977dfebaa9907c02411c9cb0f
Didier Toussaint <didier.toussaint.trad@gmail.com>
2022-09-20 10:59:59 +0200

    Minor fix for compatibility with older Node versions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const Init = require('./calls/init')
 const Sync = require('./calls/sync')
 
-const args   = process.argv.slice(2)
-const action = args[0]
-const config = args.find(arg => arg.includes('--config'))?.split('=')[1]
-const flags  = {
+const args       = process.argv.slice(2)
+const action     = args[0]
+const config     = args.find(arg => arg.includes('--config'))
+const configFile = config ? config.split('=')[1] : undefined
+const flags      = {
   readonly: args.includes('--readonly'),
   purge:    args.includes('--purge')
 }
 
 if (action == 'init') {
-  let initService = new Init(config, flags)
+  let initService = new Init(configFile, flags)
   initService.run()
 }
 else if (action == 'sync') {
-  let syncService = new Sync(config, flags)
+  let syncService = new Sync(configFile, flags)
   syncService.run()
 }
 else {
